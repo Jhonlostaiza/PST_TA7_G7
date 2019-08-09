0bc9e13189becb45feab1012c97f87b7af0541b5
Jancarlo Valencia <jananval@espol.edu.ec>
2019-08-09 17:00:37 -0500

    CAMBIOS

--- app/src/main/java/com/example/moneyapp/RegistroIngresos.java
+++ app/src/main/java/com/example/moneyapp/RegistroIngresos.java
@@ -1,14 +1,14 @@
 package com.example.moneyapp;
 
-import android.content.Context;
-import android.net.Uri;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
-
 import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 
 /**
@@ -19,92 +19,53 @@ import android.view.ViewGroup;
  * Use the {@link RegistroIngresos#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class RegistroIngresos extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+public class RegistroIngresos extends Fragment implements View.OnClickListener{
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private Button b;
+    private EditText fecha, ingreso;
+    private int codigo =0;
+    View view;
 
-    private OnFragmentInteractionListener mListener;
-
-    public RegistroIngresos() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment RegistroIngresos.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static RegistroIngresos newInstance(String param1, String param2) {
-        RegistroIngresos fragment = new RegistroIngresos();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        view = inflater.inflate(R.layout.fragment_registro_ingresos, container, false );
+
+        b = (Button) view.findViewById(R.id.registrarIngreso);
+        ingreso = (EditText) view.findViewById(R.id.editText);
+        fecha = (EditText) view.findViewById(R.id.editText2);
+
+        b.setOnClickListener(this);
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_registro_ingresos, container, false);
+        return view;
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
+    public void registrarIngreso(View view) {
+        AdminSQLOpenHelper admin = new AdminSQLOpenHelper(getContext(), "ingresos", null, 1 );
+        SQLiteDatabase bd = admin.getWritableDatabase();
+        codigo = codigo + 1;
+        String cod = Integer.toString(codigo);
+        String date = fecha.getText().toString();
+        String valor = ingreso.getText().toString();
+
+
+
+        bd.execSQL("insert into ingresos (codigo, fecha, ingreso) values ("+cod+",'"+date+"',"+valor+")");
+        bd.close();
+
+
+        fecha.setText("");
+        ingreso.setText("");
+        Toast.makeText(getContext(), "Se cargaron los datos correctamente",Toast.LENGTH_SHORT).show();
     }
 
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
+    public void onClick(View view) {
+        registrarIngreso(view);
+
     }
 
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
-    }
 }
